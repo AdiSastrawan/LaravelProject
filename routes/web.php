b49c3bf3f46259cedd487d9d83ab2db0b4f8cda0
AdiSastrawan <adisastrawan110703@gmail.com>
2022-12-11 15:08:15 +0800

    configure role and importing template

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -31,3 +30,4 @@ Route::get('login-acc', function () {
 Route::get('/home' , [PageController::class, 'landingPage'])->name('home');
 Route::get('/booking' , [PageController::class, 'bookingPage'])->name('booking');
 Route::get('/booking-details' , [PageController::class, 'bookingDetailsPage'])->name('booking-details');
+Route::get('/' , [AdminController::class, 'index'])->name('admin-page')->middleware('auth','isadmin');
